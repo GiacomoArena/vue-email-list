591962421f4617a95d1de316868b4b592e3e2bdf
GiacomoArena <giacomoarena28@gmail.com>
2023-04-21 15:15:07 +0200

    update commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const {createApp} = Vue;
+
+createApp({
+
+  data(){
+    return{
+      apiUrl:'https://flynn.boolean.careers/exercises/api/random/mail',
+    }
+  },
+
+  methods:{
+
+    getApi(){
+
+      axios.get(this.apiUrl).then(result => {
+        console.log(result.data.response);
+      })
+    }
+  },
+  mounted(){
+    this.getApi()
+
+  }
+
+
+
+}).mount('#app')
